00e05d5d934b819f748449e0c6bc70bd93e9295f
mohammadreza <mmdrza77@gmail.com>
2024-08-10 11:14:27 +0330

    cleaning up code

--- OtpInput.vue
+++ OtpInput.vue
@@ -17,8 +17,8 @@
 </template>
 
 <script setup lang="ts">
-name: 'OtpInput';
-import { computed, onBeforeUpdate, ref, watch } from 'vue';
+defineOptions({ name: "OtpInput" });
+import { computed, onBeforeUpdate, ref, watch } from "vue";
 
 // ------ Lifecycle Hooks ------
 onBeforeUpdate(() => (fields.value = []));
@@ -33,31 +33,33 @@ const fields = ref([]);
 const fieldValues = ref([]);
 const model = defineModel();
 
+// ------ Props ------
 const props = withDefaults(defineProps<IOtp>(), {
   inputLength: 5,
 });
 
+// ------ Computed ------
 const composite = computed(() => {
-  const nonNullFields = fieldValues.value.filter((value) => value);
-  if (props.inputLength !== nonNullFields.length) return '';
-  return nonNullFields.join('');
+  const nonNullFields = fieldValues.value.filter(Boolean);
+  return props.inputLength === nonNullFields.length
+    ? nonNullFields.join("")
+    : "";
 });
 
+// ------ Watch ------
 watch(composite, (newVal) => {
   if (newVal) model.value = newVal;
 });
 
+// ------ Methods ------
 const updateFieldRef = (element, index: number) => {
   if (element) fields.value[index] = element;
 };
 
 const focus = (index: number) => {
-  if (index >= 0) {
-    if (index < props.inputLength) fields.value[index].select();
-    else {
-      if (composite.value) fields.value[index - 1].blur();
-    }
-  }
+  if (index < 0) return;
+  if (index < props.inputLength) fields.value[index].select();
+  else if (composite.value) fields.value[index - 1].blur();
 };
 
 const onUpdate = (value, index: number) => {
@@ -66,9 +68,10 @@ const onUpdate = (value, index: number) => {
 
 const onKeyUp = (evnt, index: number) => {
   const key = evnt.key;
-  if (['Tab', 'Shift', 'Meta', 'Control', 'Alt'].includes(key)) return;
-  if (['Delete'].includes(key)) return;
-  if (key === 'ArrowLeft' || key === 'Backspace') focus(index - 1);
-  else if (key === 'ArrowRight') focus(index + 1);
+  const navigationKeys = ["ArrowLeft", "ArrowRight", "Backspace"];
+  if (navigationKeys.includes(key)) {
+    if (key === "ArrowLeft" || key === "Backspace") focus(index - 1);
+    else if (key === "ArrowRight") focus(index + 1);
+  }
 };
 </script>
